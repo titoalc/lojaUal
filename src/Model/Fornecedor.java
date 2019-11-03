825bbcd74d159543ffbdd722beb454c7c147e100
LuigiGenari <ludgenari@gmail.com>
2019-11-02 22:18:12 -0300

    commit fornecedor

---
+++ src/Model/Fornecedor.java
@@ -0,0 +1,128 @@
+package Model;
+
+public class Fornecedor {
+ 
+    private String nomeFornecedor;
+    private String endereco;
+    private String cep;
+    private String cidade;
+    private String estado;
+    private String telefone;
+    private String email;
+    private String nomeProduto;
+    private String marca;
+    private String codigo;
+
+    public String getNomeFornecedor() {
+        return nomeFornecedor;
+    }
+
+    public void setNomeFornecedor(String nomeFornecedor) {
+        this.nomeFornecedor = nomeFornecedor;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Fornecedor{" +
+                "nomeFornecedor='" + nomeFornecedor + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", cep='" + cep + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", estado='" + estado + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                ", nomeProduto='" + nomeProduto + '\'' +
+                ", marca='" + marca + '\'' +
+                ", codigo='" + codigo + '\'' +
+                '}';
+    }
+
+    public Fornecedor(String nomeFornecedor, String endereco, String cep, String cidade, String estado, String telefone, String email, String nomeProduto, String marca, String codigo) {
+        this.nomeFornecedor = nomeFornecedor;
+        this.endereco = endereco;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.telefone = telefone;
+        this.email = email;
+        this.nomeProduto = nomeProduto;
+        this.marca = marca;
+        this.codigo = codigo;
+
+
+
+
+    }
+}
